e4f50da285f9c100d0e95a338afd0a4ab0781b9d
roger <rogerwenst107@gmail.com>
2026-08-21 10:46:50 +0800

    Configure JoinMate Firebase project

--- firebase-version/public/firebase-config.js
+++ firebase-version/public/firebase-config.js
@@ -1,10 +1,10 @@
 // Firebase 網頁設定不是密碼，可以公開在前端。
 // 請到 Firebase Console → 專案設定 → 你的應用程式 → SDK 設定與配置複製內容。
 export const firebaseConfig = {
-  apiKey: "PASTE_YOUR_API_KEY",
-  authDomain: "PASTE_YOUR_PROJECT.firebaseapp.com",
-  projectId: "PASTE_YOUR_PROJECT_ID",
-  storageBucket: "PASTE_YOUR_PROJECT.firebasestorage.app",
-  messagingSenderId: "PASTE_YOUR_SENDER_ID",
-  appId: "PASTE_YOUR_APP_ID"
+  apiKey: "AIzaSyDr-V04h77b10QdP94V0TQg5O5LqHqG0_g",
+  authDomain: "joinmate-fire.firebaseapp.com",
+  projectId: "joinmate-fire",
+  storageBucket: "joinmate-fire.firebasestorage.app",
+  messagingSenderId: "37431721788",
+  appId: "1:37431721788:web:cb7a7c252fb0a502c5a839"
 };
